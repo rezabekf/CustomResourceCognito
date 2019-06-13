767d60df97758c2066328c963d5f2225bfaa3819
Franco Rezabek <rezabekf@amazon.co.uk>
2019-06-13 15:43:53 +0100

    Cleanup lambda code.

--- cognito_app/app.py
+++ cognito_app/app.py
@@ -12,67 +12,48 @@ client = boto3.client('cognito-idp')
 def create(event, context):
     logger.info("Got Create")
 
-    user_pool_id, client_name, provider_name = _get_properties(event)
-    client_id = ''
+    user_pool_id, client_name, provider_name, callback_urls, logout_urls = _get_resource_properties(event)
 
-    if not client_name:
-        raise Exception('Must provide a "ClientName" value in properties.')
-
-    response_create_user_pool_client = client.create_user_pool_client(
-        UserPoolId=user_pool_id,
-        ClientName=client_name,
-        GenerateSecret=True
-    )
-    logger.info(response_create_user_pool_client)
-
-    response_list_user_pool_clients = client.list_user_pool_clients(
-        UserPoolId=user_pool_id
-    )
-    for i in response_list_user_pool_clients['UserPoolClients']:
-        if i['ClientName'] == client_name:
-            client_id = i['ClientId']
-
-    response_update_user_pool_client = client.update_user_pool_client(
-        UserPoolId=user_pool_id,
-        ClientId=client_id,
-        SupportedIdentityProviders=[
-            provider_name,
-        ],
-        CallbackURLs=[
-            'https://application.url/login',
-        ],
-        LogoutURLs=[
-            'https://application.url/logout'
-        ],
-        AllowedOAuthFlows=[
-            'code'
-        ],
-        AllowedOAuthScopes=[
-            'phone',
-            'email',
-            'openid'
-        ],
-        AllowedOAuthFlowsUserPoolClient=True
-    )
-    logger.info(response_update_user_pool_client)
-
-    helper.Data.update({"ClientName": client_name})
-
-    return helper.PhysicalResourceId
+    return _create(user_pool_id, client_name, provider_name, callback_urls, logout_urls)
 
 
 @helper.update
 def update(event, context):
     logger.info("Got Update")
 
-    user_pool_id, client_name, provider_name = _get_properties(event)
-    client_id = ''
+    user_pool_id, client_name, provider_name, callback_urls, logout_urls = _get_resource_properties(event)
     old_client_name = event['OldResourceProperties']['ClientName']
-    old_client_id = ''
 
-    if not client_name:
-        raise Exception('Must provide a "ClientName" value in properties.')
+    _delete(user_pool_id, old_client_name)
 
+    return _create(user_pool_id, client_name, provider_name, callback_urls, logout_urls)
+
+
+@helper.delete
+def delete(event, context):
+    logger.info("Got Delete")
+
+    user_pool_id, client_name, provider_name, callback_urls, logout_urls = _get_resource_properties(event)
+
+    _delete(user_pool_id, client_name)
+
+
+def handler(event, context):
+    helper(event, context)
+
+
+def _get_resource_properties(event):
+    properties = event.get('ResourceProperties', None)
+    user_pool_id = properties.get('UserPoolId')
+    client_name = properties.get('ClientName')
+    provider_name = properties.get('ProviderName')
+    callback_urls = properties.get('CallbackURLs')
+    logout_urls = properties.get('LogoutURLs')
+
+    return user_pool_id, client_name, provider_name, callback_urls, logout_urls
+
+
+def _create(user_pool_id, client_name, provider_name, callback_urls, logout_urls):
     response_create_user_pool_client = client.create_user_pool_client(
         UserPoolId=user_pool_id,
         ClientName=client_name,
@@ -83,31 +64,18 @@ def update(event, context):
     response_list_user_pool_clients = client.list_user_pool_clients(
         UserPoolId=user_pool_id
     )
-    for i in response_list_user_pool_clients['UserPoolClients']:
-        if i['ClientName'] == client_name:
-            client_id = i['ClientId']
-        elif i['ClientName'] == old_client_name:
-            old_client_id = i['ClientId']
 
-    response_delete_user_pool_client = client.delete_user_pool_client(
-        UserPoolId=user_pool_id,
-        ClientId=old_client_id
-    )
-    logger.info(response_delete_user_pool_client)
+    client_id = next(
+        x['ClientId'] for x in response_list_user_pool_clients['UserPoolClients'] if x['ClientName'] == client_name)
 
     response_update_user_pool_client = client.update_user_pool_client(
         UserPoolId=user_pool_id,
         ClientId=client_id,
-        ClientName=client_name,
         SupportedIdentityProviders=[
-            provider_name,
+            provider_name
         ],
-        CallbackURLs=[
-            'https://application.url/login',
-        ],
-        LogoutURLs=[
-            'https://application.url/logout'
-        ],
+        CallbackURLs=callback_urls,
+        LogoutURLs=logout_urls,
         AllowedOAuthFlows=[
             'code'
         ],
@@ -125,37 +93,16 @@ def update(event, context):
     return helper.PhysicalResourceId
 
 
-@helper.delete
-def delete(event, context):
-    logger.info("Got Delete")
-
-    user_pool_id, client_name, provider_name = _get_properties(event)
-    client_id = ''
-
+def _delete(user_pool_id, client_name):
     response_list_user_pool_clients = client.list_user_pool_clients(
         UserPoolId=user_pool_id
     )
 
-    for i in response_list_user_pool_clients['UserPoolClients']:
-        if i['ClientName'] == client_name:
-            client_id = i['ClientId']
+    client_id = next(
+        x['ClientId'] for x in response_list_user_pool_clients['UserPoolClients'] if x['ClientName'] == client_name)
 
     response_delete_user_pool_client = client.delete_user_pool_client(
         UserPoolId=user_pool_id,
         ClientId=client_id
     )
     logger.info(response_delete_user_pool_client)
-
-
-def handler(event, context):
-    helper(event, context)
-
-
-def _get_properties(event):
-    properties = event.get('ResourceProperties', None)
-
-    user_pool_id = properties.get('UserPoolId')
-    client_name = properties.get('ClientName')
-    provider_name = properties.get('ProviderName')
-
-    return user_pool_id, client_name, provider_name
--- cognito_domain/domain.py
+++ cognito_domain/domain.py
@@ -5,18 +5,57 @@ from crhelper import CfnResource
 
 logger = logging.getLogger(__name__)
 helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL')
-
 client = boto3.client('cognito-idp')
 
 
 @helper.create
 def create(event, context):
     logger.info("Got Create")
-    domain_name, user_pool_id = _get_properties(event)
+
+    domain_name, user_pool_id = _get_resource_properties(event)
 
     if not domain_name:
         raise Exception('Must provide a "Domain" value in properties.')
 
+    return _create(domain_name, user_pool_id)
+
+
+@helper.update
+def update(event, context):
+    logger.info("Got Update")
+
+    domain_name, user_pool_id = _get_resource_properties(event)
+    old_domain = event['OldResourceProperties']['DomainName']
+
+    if old_domain != domain_name:
+        _delete(old_domain, user_pool_id)
+
+    return _create(domain_name, user_pool_id)
+
+
+@helper.delete
+def delete(event, context):
+    logger.info("Got Delete")
+
+    domain_name, user_pool_id = _get_resource_properties(event)
+
+    _delete(domain_name, user_pool_id)
+
+
+def handler(event, context):
+    helper(event, context)
+
+
+def _get_resource_properties(event):
+    properties = event.get('ResourceProperties', None)
+
+    domain_name = properties.get('DomainName')
+    user_pool_id = properties.get('UserPoolId')
+
+    return domain_name, user_pool_id
+
+
+def _create(domain_name, user_pool_id):
     response = client.create_user_pool_domain(
         Domain=domain_name,
         UserPoolId=user_pool_id
@@ -26,48 +65,9 @@ def create(event, context):
     return helper.PhysicalResourceId
 
 
-@helper.update
-def update(event, context):
-    logger.info("Got Update")
-    domain_name, user_pool_id = _get_properties(event)
-    old_domain = event['OldResourceProperties']['DomainName']
-
-    if old_domain != domain_name:
-        response_delete = client.delete_user_pool_domain(
-            Domain=old_domain,
-            UserPoolId=user_pool_id
-        )
-
-        response_create = client.create_user_pool_domain(
-            Domain=domain_name,
-            UserPoolId=user_pool_id
-        )
-        helper.Data.update({"DomainName": domain_name})
-
-    return helper.PhysicalResourceId
-
-
-@helper.delete
-def delete(event, context):
-    logger.info("Got Delete")
-    domain_name, user_pool_id = _get_properties(event)
-
-    response = client.delete_user_pool_domain(
+def _delete(domain_name, user_pool_id):
+    response_delete_user_pool_domain = client.delete_user_pool_domain(
         Domain=domain_name,
         UserPoolId=user_pool_id
     )
-
-    return helper.PhysicalResourceId
-
-
-def handler(event, context):
-    helper(event, context)
-
-
-def _get_properties(event):
-    properties = event.get('ResourceProperties', None)
-
-    domain_name = properties.get('DomainName')
-    user_pool_id = properties.get('UserPoolId')
-
-    return domain_name, user_pool_id
+    logger.info(response_delete_user_pool_domain)
